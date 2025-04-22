6766ba1820f9401b6bd57d5827c80ecc39460bfe
Ibtasam <quantumbytes.tech@gmail.com>
2025-04-22 21:55:54 +0700

    commit

--- model_training.py
+++ model_training.py
@@ -15,7 +15,6 @@ def train_and_evaluate_model(X, y):
     # Split data
     X_train, X_test, y_train, y_test = train_test_split(X_poly, y, test_size=0.2, random_state=42)
 
-    # Random Forest model
     model = RandomForestClassifier(random_state=42)
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
